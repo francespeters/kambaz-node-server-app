e17e9a0a7342457844fdf70596672357b9ece77e
francespeters <peters.f@northeastern.edu>
2026-03-26 16:14:40 -0400

    Part 5.2.4-5.4.2

---
+++ kambaz/database/assignments.js
@@ -0,0 +1,9 @@
+export default[
+  { _id: "A101", title: "Propulsion Assignment", course: "RS101", description: "Assignment on rocket propulsion principles.", points: "100" , dueDate: "2024-12-01", availableFrom: "2024-11-01", availableUntil: "2024-12-01" },
+  { _id: "A102", title: "Combustion Analysis", course: "RS101", description: "Assignment on combustion processes.", points: "100" , dueDate: "2024-12-01", availableFrom: "2024-11-01", availableUntil: "2024-12-01" },
+  { _id: "A103", title: "Nozzle Design Project", course: "RS101", description: "Project on nozzle design and optimization.", points: "100" , dueDate: "2024-12-01", availableFrom: "2024-11-01", availableUntil: "2024-12-01" },
+  { _id: "A201", title: "Aerodynamics Quiz", course: "RS102", description: "Quiz on aerodynamic principles.", points: "100" , dueDate: "2024-12-01", availableFrom: "2024-11-01", availableUntil: "2024-12-01" },
+  { _id:"A202",__title__:"Flow Analysis","course":"RS102","description":"Analysis of fluid flow characteristics.","points":"100","dueDate":"2024-12-01","availableFrom":"2024-11-01","availableUntil":"2024-12-01" },
+  { _id:"A203","title":"Heating Analysis","course":"RS102","description":"Analysis of heating processes and heat transfer.","points":"100","dueDate":"2024-12-01","availableFrom":"2024-11-01","availableUntil":"2024-12-01" },
+  { _id: "A301", title: "Structural Design Task", course: "RS102", description: "Task on structural design principles.", points: "100" , dueDate: "2024-12-01", availableFrom: "2024-11-01", availableUntil:("2024-12-01") }
+]
